48e43b19037cf1520a92fba66a1dae41c5de4599
zhengxiaoyan <zhang.bing.xy@gmail.com>
2013-04-03 22:28:34 +0900

    save change

--- JSPanel.js
+++ JSPanel.js
@@ -1,3 +1,24 @@
 /**
  * @author zhang bing
  */
+(function(ZB) {
+	JSPanel = function() {
+	
+		Panel = function( options ) {
+			// TODO
+		}
+		
+		Panel.prototype = {
+			// TODO
+		}
+		
+		Component = function( options ) {
+			// TODO
+		}
+		
+		Component.prototype = {
+			// TODO
+		}
+	}
+
+})(ZB || {})
